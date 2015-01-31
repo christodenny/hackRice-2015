6eb1058d90f0843b2539d2148a59dab2f992fae6
JacobVanGeffen <jsvangeffen@gmail.com>
2015-01-31 16:01:59 -0600

    Android stuff

---
+++ Android/Pursonal/src/com/hack/rice/pursonal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hack.rice.pursonal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+
+}
